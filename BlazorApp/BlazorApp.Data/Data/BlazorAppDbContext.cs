927a6c784d5301ea9ccfd0aceafb1015eeea5da7
Hardik Gohil <hardik.adwebsoft@gmail.com>
2023-03-09 20:15:52 +0530

    Seed Data

--- BlazorApp/BlazorApp.Data/Data/BlazorAppDbContext.cs
+++ BlazorApp/BlazorApp.Data/Data/BlazorAppDbContext.cs
@@ -10,14 +10,20 @@ namespace BlazorApp.Data.Data
 {
     public class BlazorAppDbContext : DbContext
     {
-        //public BlazorAppDbContext(DbContextOptions<BlazorAppDbContext> dbContextOptions)
-        //{
-
-        //}
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() { Id = 1, Name = "John", Email = "johndoe@example.com", DateOfBirth = "2 jan 1986", Department = "IT" },
+                new Employee() { Id = 2, Name = "Jack", Email = "jack@example.com", DateOfBirth = "2 jan 1986", Department = "Marketing" },
+                new Employee() { Id = 3, Name = "Sam", Email = "dank@example.com", DateOfBirth = "4 jan 1987", Department = "Admin" },
+               new Employee() { Id = 4, Name = "Paul", Email = "paul@example.com", DateOfBirth = "5 jan 1989", Department = "Marketing" }
+            );
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseInMemoryDatabase(databaseName: "EmployeeDb");
         }
-        public DbSet<Employee> Employees { get; set; }
+       
     }
 }
